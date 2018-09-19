e597a2e99eead177f3cf5d757dc6e208c2e0f59e
mb480s <mike.f.bagheri@gmail.com>
2018-09-19 15:04:30 -0400

    picture/timer bug's fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,10 @@ var questions1 = {
             B1: 'Green',
             C1: 'Purple',
             D1: 'Orange',
-            Ans1: 'Green',            
+            Ans1: 'Green',  
+            pic: 'assets/image/greenApple.jpg',
+            expired: false,
+
 }
 
 var questions2 = {
@@ -21,7 +24,9 @@ var questions2 = {
             B1: 'Green',
             C1: 'Purple',
             D1: 'Orange',
-            Ans1: 'Red',            
+            Ans1: 'Red', 
+            pic: 'assets/image/greenApple.jpg',
+            expired: false,           
 }
 
 var questions3 = {
@@ -30,42 +35,36 @@ var questions3 = {
             B1: 'Green',
             C1: 'Purple',
             D1: 'Orange',
-            Ans1: 'Blue',            
+            Ans1: 'Blue', 
+            pic: 'assets/image/greenApple.jpg',
+            expired: false,           
 }
 
 var questions = [questions1,questions2,questions3];
-console.log(questions)
+
 
 //this object keeps score/win/loss
 var gameScore = {
     score: 0,
-    wrongAnswer: 0,    
+    wrongAnswer: 0, 
+    unanswered: 0,   
     submitted: function(guess) {
         clearInterval(intervalId);
         clockRunning = false;
-         
+        $('#timer').empty() 
+        $('.answerOne').empty()
+        $('#imageContainer').append(
+            `
+            <img src="${questions[i].pic}" alt="Green Apple pic" style = "border: 1px solid black">
+            `
+        )
         if (guess === questions[i].Ans1) {
-            $('#timer').empty()
             gameScore.score++;
+            
             $('.numRightAnswers').text('Correct Answers: ' + gameScore.score)
-            i++;
-            time = 10;
-            if (i < questions.length){
-                displayQuestions()
-            } else {
-                i = 0;
-                $('#timer').empty() 
-                $('.questionOne').empty()
-                $('.answerOne').empty()
-                $('#correctAnswerDisplayOne').empty()
-                $('#gameButton').append(`
-                <button id="startGame">Play Again</button>
-                `)
-                $('#outcome').text('You Win!')
-                gameScore.wrongAnswer = 0
-                gameScore.score = 0
-
-            }            
+            $('#correctAnswerDisplayOne').text("Correct!!!")
+            setTimeout(generalDelay,2000)  
+                       
         }
         else {
             gameScore.wrongAnswer++;
@@ -74,12 +73,9 @@ var gameScore = {
             $('#timer').empty()
             setTimeout(generalDelay,3000)       
         }
-     
-
-       
-
     }    
 }
+
 
 // Click button to start game, displays questions and starts/displays timer
 $(document).on('click', '#startGame', function() {
@@ -87,43 +83,21 @@ $(document).on('click', '#startGame', function() {
     $('#outcome').empty()
     $('.numRightAnswers').empty();
     $('.numWrongAnswers').empty();
+    $('#imageContainer').empty()
+    gameScore.wrongAnswer = 0
+    gameScore.score = 0
     displayQuestions()
 })
 
 //capturing which answer is clicked by user
-$(document).on('click', '#answerA', function() {
-   if (!answerPicked){
-    var guess = $(this).attr('data-val')
-    gameScore.submitted(guess); 
-    answerPicked = true
-}
-})
-
-$(document).on('click', '#answerB', function() {
+$(document).on('click', '.answers', function() {
     if (!answerPicked){
-        var guess = $(this).attr('data-val')
-        gameScore.submitted(guess); 
-        answerPicked = true
-    }
-})
-
-$(document).on('click', '#answerC', function() {
-    if (!answerPicked){
-        var guess = $(this).attr('data-val')
-        gameScore.submitted(guess); 
-        answerPicked = true
-    }
-})
-
-$(document).on('click', '#answerD', function() {
-    if (!answerPicked){
-        var guess = $(this).attr('data-val')
-        gameScore.submitted(guess); 
-        answerPicked = true
-    } 
-})
-
-
+     var guess = $(this).attr('data-val')
+     console.log(guess)
+     gameScore.submitted(guess); 
+     answerPicked = true
+ }
+ })
 
 
 
@@ -132,6 +106,7 @@ $(document).on('click', '#answerD', function() {
 // this function displays the question and multiple choice answers on the DOM
 // and starts the count down timer by invoking the startclock function
 function displayQuestions(){
+    
         $('.questionOne').text(questions[i].Q1)
         $('.answerOne').empty()
         $('#correctAnswerDisplayOne').empty()
@@ -158,12 +133,19 @@ function displayQuestions(){
          if (time < 1){
             clearInterval(intervalId);
             clockRunning = false
-             console.log('time out')
+            $('#timer').empty() 
+            $('.answerOne').empty()
+            $('#imageContainer').append(
+                `
+                <img src="${questions[i].pic}" alt="Green Apple pic" style = "border: 1px solid black">
+                `
+            )
              $('#correctAnswerDisplayOne').text('The correct answer was: ' + questions[i].Ans1)
              $('countdownTimer').append(`
                 <p id="timer">${time}<p>
              `)
-             gameScore.wrongAnswer++;
+             gameScore.unanswered++
+             gameScore.wrongAnswer++
             $('.numWrongAnswers').text('Wrong Answers: ' +gameScore.wrongAnswer)
              setTimeout(generalDelay,3000)
         }
@@ -171,10 +153,11 @@ function displayQuestions(){
     }
       
     function generalDelay() {
-        console.log('in general delay')
-        i++;
+            i++;
             time = 10;
             if (i < questions.length){
+                $('.questionOne').empty()
+                $('#imageContainer').empty()
                 displayQuestions()
             } else {
                 i = 0;
@@ -182,14 +165,42 @@ function displayQuestions(){
                 $('.questionOne').empty()
                 $('.answerOne').empty()
                 $('#correctAnswerDisplayOne').empty()
-                $('#gameButton').append(`
-                <button id="startGame">Play Again</button>
-                `)
-                $('#outcome').text('You Lose!')
-                gameScore.wrongAnswer = 0
-                gameScore.score = 0
-            }
-        
+                $('#imageContainer').empty()
+                let percentage = gameScore.score / questions.length * 100
+                console.log(percentage)
+                if(percentage < 90) {
+                    $('#outcome').text('You Lose!')
+                    $('#gameButton').append(`
+                        <button id="startGame">Play Again</button>
+                        `)
+                } else {
+                    $('#outcome').text('You Win!')
+                    $('#gameButton').append(`
+                        <button id="startGame">Play Again</button>
+                        `)
+                }
+            }   
     }
 
+
+
+    // function generalDelay1() {
+    //     i++;
+    //     time = 10;
+    //         if (i < questions.length){
+    //             $('.questionOne').empty()
+    //             $('#imageContainer').empty()
+    //             displayQuestions()
+    //         } else {
+    //             i = 0;
+
+    //             $('#correctAnswerDisplayOne').empty()
+    //             $('#gameButton').append(`
+    //             <button id="startGame">Play Again</button>
+    //             `)
+    //             $('#outcome').text('You Win!')
+    //             gameScore.wrongAnswer = 0
+    //             gameScore.score = 0
+    //         } 
+    // }
 })
